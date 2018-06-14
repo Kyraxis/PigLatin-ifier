a2f29f5e5231cf291e0e55977a843b3752b1ca80
Kyraxis <39130577+Kyraxis@users.noreply.github.com>
2018-06-13 18:45:48 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,68 @@
 # Specific uses in Pig-Latin
-def t(str):
-  #Returns the 2 letters added together to check against
-  return str[0]+str[1]        
+def add_special_letters(str):
+    # Returns the 2 letters added together to check against
+    return str[0] + str[1]
 
-# Speicfic use characters                
-lst = ['sh', 'gl', 'ch', 'ph', 'tr', 'br', 'fr', 'bl', 'gr', 'st', 'sl', 'cl', 'pl', 'fl']
+def parse_sentence(sentence):
+    special_letters = ['sh', 'gl', 'ch', 'ph', 'tr', 'br', 'fr', 'bl',
+                       'gr', 'st', 'sl', 'cl', 'pl', 'fl']
+
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    suffix = 'ay'
+
+    new_sentence = []
+    for i in sentence.split():
+        # Checks for any NON alphabetical characters
+        new_word = ""
+        if i.isalpha():
+            if i[0] in vowels:
+                new_word = i + suffix
+            # Checking for the special_letters up top (All of the combined character sounds starting a word)
+            elif add_special_letters(i) in special_letters:
+                new_word = i[2:]+i[:2] + suffix
+            # ANYTHING ELSE!
+            else:
+                new_word = i[1:]+i[0] + suffix
+
+        else:
+            # Checks for any digits in the input string
+            if i[0].isdigit():
+                new_word = i
+            # Checks for . ? or ! because damn
+            elif i[-1] == "." or i[-1] == "?" or i[-1] == "!":
+                if i[0] in vowels:
+                    new_word = i[0:-1] + suffix + i[-1]
+                elif add_special_letters(i) in special_letters:
+                    new_word = i[2:-1]+i[:2] + suffix + i[-1]
+                else:
+                    new_word = i[1:-1]+i[0] + suffix + i[-1]
+            # Check for vowel start
+            elif i[0] in vowels:
+                if i.find("'") or i.find("-"):
+                    new_word = i+suffix
+                else:
+                    new_word = i[0:-1]+suffix+i[-1]
+            # Check for special characters
+            elif add_special_letters(i) in special_letters:
+                if i.find("'") or i.find("-"):
+                    new_word = i[2:]+i[:2]+suffix
+                else:
+                    new_word = i[2:-1]+i[:2]+suffix+i[-1]
+            # Yeah, everything else
+            else:
+                if i.find("'") or i.find("-"):
+                    new_word = i[1:]+i[0]+suffix
+                else:
+                    new_word = i[1:-1]+i[0]+suffix+i[-1]
+        new_sentence.append(new_word)
+    return ' '.join(new_sentence)
+
 # My input section
 sentence = input('Type what you would like translated into pig-latin and press ENTER: ')
 # sentence.split() converts the sentence into a list per word
-sentence = sentence.split()
-for k in range(len(sentence)):
-  i = sentence[k]
-  #Checks for any NON alphabetical characters
-  if i.isalpha() == False:
-    if i[0].isdigit() == True:
-      sentence[k] = i
-    elif i[0] in ['a', 'e', 'i', 'o', 'u']:
-      if i.find("'") or i.find("-"):
-        sentence[k] = i+'ay'
-      else:
-        sentence[k] = i[0:-1]+'ay'+i[-1]
-    elif t(i) in lst:
-      if i.find("'") or i.find("-"):
-        sentence[k] = i[2:]+i[:2]+'ay'
-      else:
-        sentence[k] = i[2:-1]+i[:2]+'ay'+i[-1]
-    else:
-      if i.find("'") or i.find("-"):
-        sentence[k] = i[1:]+i[0]+'ay'
-      else:
-        sentence[k] = i[1:-1]+i[0]+'ay'+i[-1]
-  #Apparently isalpha() does not account for . or ? at the end of a sentence, so I did.
-  elif i[-1] == "." or i[-1] == "?":
-    if i[0] in ['a', 'e', 'i', 'o', 'u']:
-      sentence[k] = i[0:-1]+'ay'+i[-1]
-    elif t(i) in lst:
-      sentence[k] = i[2:-1]+i[:2]+'ay'+i[-1]
-    else:
-      sentence[k] = i[1:-1]+i[0]+'ay'+i[-1]
-  #Checking for vowels    
-  elif i[0] in ['a', 'e', 'i', 'o', 'u']:
-    sentence[k] = i+'ay'
-  #Checking for the lst up top (All of the combined character sounds starting a word)
-  elif t(i) in lst:
-    sentence[k] = i[2:]+i[:2]+'ay'
-  # ANYTHING ELSE!
-  else:
-    sentence[k] = i[1:]+i[0]+'ay'
-#Let's print this thing    
-new_string = ' '.join(sentence)
-print(new_string)
+
+# Let's print this thing
+new_sentence = parse_sentence(sentence)
+print("Original Sentence: " + sentence.capitalize())
+print("Pig Latin-ified: " + new_sentence.capitalize())
+
